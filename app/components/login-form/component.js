599b9234ee27eb81c809688a36b1bbc561d04c02
Ian Molee <imolee@gmail.com>
2019-05-22 20:28:08 -0700

    Add login handler

--- app/components/login-form/component.js
+++ app/components/login-form/component.js
@@ -1,4 +1,22 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class LoginFormComponent extends Component {
+  loginUser(userId) {
+    console.log(userId);
+  }
+
+  /**
+   *
+   * @param {Event & {target: HTMLFormElement}} evt
+   *
+   * @action decorator ensures proper binding for calling this.loginUser
+   * this would be the form element without the @action decorator.
+   */
+  @action
+  onLoginFormSubmit(evt) {
+    evt.preventDefault();
+    const select = evt.target.querySelector('select');
+    this.loginUser(select.value);
+  }
 }
